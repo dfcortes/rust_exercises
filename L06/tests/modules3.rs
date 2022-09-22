d61d6da8fb2f48953773c674e73aa63c1f7796fe
Diego Cortes <dfer.cortes@gmail.com>
2022-09-22 07:45:37 -0500

    Solved modules3

--- L06/tests/modules3.rs
+++ L06/tests/modules3.rs
@@ -5,15 +5,14 @@
 // I AM NOT DONE
 
 // TODO: Complete this use statement
-use ???
-
 
 #[cfg(test)]
 mod tests {
+    use std::time::SystemTime;
 
     #[test]
     fn test_modules() {
-        match SystemTime::now().duration_since(UNIX_EPOCH) {
+        match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
             Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
             Err(_) => panic!("SystemTime before UNIX EPOCH!"),
         }
